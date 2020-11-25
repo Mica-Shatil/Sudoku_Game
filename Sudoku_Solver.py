775cb3970ac7d50fca3576b395043bd38cf75d43
Mica-Shatil <71103380+Mica-Shatil@users.noreply.github.com>
2020-11-24 23:21:53 -0500

    Update Sudoku_Solver.py

--- Sudoku_Solver.py
+++ Sudoku_Solver.py
@@ -4,12 +4,13 @@ from pygame.locals import *
 import time
 import random
 
+# class for boards
 class Board:
 
     def __init__(self, rows, columns):
         self.rows = rows
         self.cols = columns
-        self.num_sol = 0
+        self.num_sol = 0 # number of solutions to a board
         self.board = np.array([
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,6 +23,7 @@ class Board:
             [0, 0, 0, 0, 0, 0, 0, 0, 0]
         ])
 
+    # finds next empty square
     def find(self, brd):
         for i in range(np.shape(brd)[0]):
             for j in range(np.shape(brd)[1]):
@@ -30,6 +32,7 @@ class Board:
 
         return -1, 0, 0, 0
 
+    # checks to see if val is a valid number in row position (row, col)
     def check_val(self, val, row, col, box_row, box_col):
 
         # check row
@@ -91,11 +94,12 @@ class Board:
 
         return True
 
+    # creates a full randomized board
     def create(self):
         row, col, box_x, box_y = self.find(self.board)
         if (row == -1):
             return 1
-        rand_num = np.array([1,2,3,4,5,6,7,8,9])
+        rand_num = [1,2,3,4,5,6,7,8,9]
         random.shuffle(rand_num)
         for i in range(9):
             if (self.check_val(rand_num[i], row, col, box_x, box_y)):
@@ -105,6 +109,7 @@ class Board:
                 self.board[row][col] = 0
         return 2
 
+    # will count the number of solutions to a baord
     def solve(self):
         row, col, box_x, box_y = self.find(self.board)
         if (row == -1):
@@ -117,18 +122,18 @@ class Board:
                 self.board[row][col] = 0
         return 2
 
+    # will delete tiles of a board to create a playable board with one solution
     def delete_num(self):
-        row = [0,0,0]
-        col = [0,0,0]
-        prev_val = [0,0,0]
-        for i in range(3):
-            while (True):
-                row[i] = np.random.randint(0,9)
-                col[i] = np.random.randint(0,9)
-                if (self.board[row[i]][col[i]] != 0):
-                    prev_val[i] = self.board[row[i]][col[i]]
-                    self.board[row[i]][col[i]] = 0
-                    break
+        row = 0
+        col = 0
+        prev_val = 0
+        while (True):
+            row = np.random.randint(0,9)
+            col = np.random.randint(0,9)
+            prev_val = self.board[row][col]
+            if (self.board[row][col] != 0):
+                self.board[row][col] = 0
+                break
 
         self.solve()
 
@@ -136,18 +141,23 @@ class Board:
             self.num_sol = 0
             self.delete_num()
         else:
-            for i in range(3):
-                self.board[row[i]][col[i]] = prev_val[i]
+            self.board[row][col] = prev_val
             self.num_sol = 0
             return True
 
+# class for games
 class Game():
+    # points for display grid
     board_dim_x = [10, 77, 144, 211, 278, 345, 412, 479, 546, 613]
     board_dim_y = [60, 127, 194, 261, 328, 395, 462, 529, 596, 663]
+
     board_width = 603
     board_height = 603
-    width_shift = 10
+    
+    # distance form the end of the window and the start of the grid
+    width_shift = 10 
     height_shift = 60
+    
     BLACK = (0,0,0)
     BLUE = (0, 160, 210)
     pygame.font.init()
@@ -156,31 +166,33 @@ class Game():
     def __init__(self, board, game_board):
         self.rows = 9
         self.cols = 9
-        self.mistakes = 0
-        self.cubes = [[Tile(board[i][j], i, j, Game.board_width, Game.board_height) for j in range(9)] for i in range(9)]
-        self.selected = None
-        self.cubes_left = self.find_empties()
-        self.complete_board = game_board
+        self.mistakes = 0 # stores number of mistakes made when trying to solve the board
+        self.tiles = [[Tile(board[i][j], i, j, Game.board_width, Game.board_height) for j in range(9)] for i in range(9)] # initialzes and stores tiles
+        self.selected = None # selected tile
+        self.tiles_left = self.find_empties() # number of tiles that are still empty
+        self.complete_board = game_board # stores the full board with the empty tiles filled in
+        
+        # initialize window
         self.game_screen = pygame.display.set_mode((623,673))
         pygame.display.set_caption("Random Board Sudoku Game")
         self.game_screen.fill(Game.BLUE)
         pygame.display.update()
 
+    # counts the number of empy tiles left
     def find_empties(self):
         x = 0
         for i in range(9):
             for j in range(9):
-                if self.cubes[i][j].value == 0:
+                if self.tiles[i][j].value == 0:
                     x += 1
         
         return x
 
+    # draws and fills in the grid
     def draw_grid(self, start_time):
         self.game_screen.fill(Game.BLUE)
-
-        text = self.font_game.render("Errors: " + str(self.mistakes), 1, (0, 0, 0))
-        self.game_screen.blit(text, (10, 10))
         
+        # draws grid
         for i in range(10):
             if i ==3 or i == 6:
                 pygame.draw.lines(self.game_screen, Game.BLACK, False, [(self.board_dim_x[0], self.board_dim_y[i]), (self.board_dim_x[9], self.board_dim_y[i])], 8)
@@ -188,15 +200,20 @@ class Game():
             pygame.draw.lines(self.game_screen, Game.BLACK, False, [(self.board_dim_x[0], self.board_dim_y[i]), (self.board_dim_x[9], self.board_dim_y[i])], 5)
             pygame.draw.lines(self.game_screen, Game.BLACK, False, [(self.board_dim_x[i], self.board_dim_y[0]), (self.board_dim_x[i], self.board_dim_y[9])], 5)
 
+        # fills tiles
         for i in range(self.rows):
             for j in range(self.cols):
-                self.cubes[i][j].draw(self.game_screen)
+                self.tiles[i][j].draw(self.game_screen)
 
+        # writes headers
+        text = self.font_game.render("Errors: " + str(self.mistakes), 1, (0, 0, 0))
+        self.game_screen.blit(text, (10, 10))
         time_text = self.font_game.render(get_time(round(time.time() - start_time)), 1, (0, 0, 0))
         self.game_screen.blit(time_text, (500, 10))
 
         pygame.display.update()
 
+    # identifies the tile that a click occurs on
     def click_loc(self, pos):
         if pos[0] < 613 and pos[1] < 663:
             gap = Game.board_width / 9
@@ -206,46 +223,48 @@ class Game():
         else:
             return None
 
+    # selects the tile clicked if the tile is empty
     def select(self, row, col):
-        if self.cubes[row][col].value == 0:
+        if self.tiles[row][col].value == 0:
             for i in range(self.rows):
                 for j in range(self.cols):
-                    self.cubes[i][j].selected = False
+                    self.tiles[i][j].selected = False
             
-            self.cubes[row][col].selected = True
+            self.tiles[row][col].selected = True
             self.selected = (row, col)
 
+    # checks key is a valid value for the selected tile
     def check_num (self, key):
         if (self.complete_board[self.selected[0]][self.selected[1]] == key):
-            self.cubes_left -= 1
+            self.tiles_left -= 1
             return True
             
         return False
     
+    # updates selected tile to the correct value and deselects it
     def update_cube (self, key):
-        self.cubes[self.selected[0]][self.selected[1]].value = key
-        self.cubes[self.selected[0]][self.selected[1]].selected = False
+        self.tiles[self.selected[0]][self.selected[1]].value = key
+        self.tiles[self.selected[0]][self.selected[1]].selected = False
         self.selected = None
 
-    def update_display(self, scrn):
-        text = self.font_game.render(str(self.mistakes), 1, (0, 0, 0))
-        scrn.blit(text, (10, 0))
-
+    # increases mistakes count
     def fail (self):
         self.mistakes += 1
 
+#class for tile
 class Tile():
     rows = 9
     columns = 9
 
     def __init__(self, value, row, col, width, height):
-        self.value = value
+        self.value = value # number value of the tile
         self.row = row
         self.col = col
         self.width = width
         self.height = height
-        self.selected = False
+        self.selected = False # whether or not the tile is selected
 
+    # draws the value of the tile and highligth if it is selected
     def draw(self, win):
         font = pygame.font.Font('freesansbold.ttf', 40)
 
@@ -260,12 +279,7 @@ class Tile():
         if self.selected:
             pygame.draw.rect(win, (255,0,0), (x, y, gap, gap), 3)
 
-    def set(self, val):
-        self.value = val
-
-    def set_temp(self, val):
-        self.temp = val
-
+# gets the current game time
 def get_time(secs):
     sec = secs%60
     minute = secs//60
@@ -273,12 +287,13 @@ def get_time(secs):
     mat = str(minute) + ":" + str(sec)
     return mat
 
+# runs the victory sequence
 def board_completed(game1, start_time):
     endgame = False
     game1.game_screen.fill(Game.BLUE)
     text1 = game1.font_game.render("You Win With " + str(game1.mistakes) + " Mistakes", 1, (0, 0, 0))
     game1.game_screen.blit(text1, (10, 200))
-    text2 = game1.font_game.render("And In " + get_time(round (time.time() - start_time)) + " Time", 1, (0, 0, 0))
+    text2 = game1.font_game.render("And In " + get_time(round (time.time() - start_time)) + " Min", 1, (0, 0, 0))
     game1.game_screen.blit(text2, (10, 250))
     pygame.display.update()
 
@@ -289,34 +304,34 @@ def board_completed(game1, start_time):
                 endgame = True
 
 def main():
-    pygame.init()
-    pygame.font.init()
-    board1 = Board(9, 9)
-    val_board = board1.create()
-    board_game = Board(9,9)
+    board1 = Board(9, 9) # the completed board
+    board1.create()
+    board_game = Board(9,9) # the part empty board
     board_game.board = board1.board.copy()
     board_game.delete_num()
-    game1 = Game(board_game.board.copy(), board1.board.copy())
+    game1 = Game(board_game.board.copy(), board1.board.copy()) # creates game board
     end_game = False
-    key = None
-    start = time.time()
+    key = None # key pressed
+    start = time.time() # start time
     game1.draw_grid(start)
-    game1.update_display(game1.game_screen)
     
+    # main game loop
     while not end_game:
-        if game1.cubes_left == 0:
+        # checks if the board is completed
+        if game1.tiles_left == 0:
             board_completed(game1, start)
             end_game = True
         
         pygame.time.delay(100)
         game1.draw_grid(start)
 
-        key = None
+        key = None # reset key
         
+        # check for user interactions
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == QUIT: # endgame
                 end_game = True
-            elif event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN: # key press
                 if event.key == pygame.K_1:
                     key = 1
                 elif event.key == pygame.K_2:
@@ -335,41 +350,24 @@ def main():
                     key = 8
                 elif event.key == pygame.K_9:
                     key = 9
-                elif event.key == pygame.K_KP1:
-                    key = 1
-                elif event.key == pygame.K_KP2:
-                    key = 2
-                elif event.key == pygame.K_KP3:
-                    key = 3
-                elif event.key == pygame.K_KP4:
-                    key = 4
-                elif event.key == pygame.K_KP5:
-                    key = 5
-                elif event.key == pygame.K_KP6:
-                    key = 6
-                elif event.key == pygame.K_KP7:
-                    key = 7
-                elif event.key == pygame.K_KP8:
-                    key = 8
-                elif event.key == pygame.K_KP9:
-                    key = 9
             
-            if event.type == MOUSEBUTTONDOWN:
+            if event.type == MOUSEBUTTONDOWN: # mouse press
                 pos = pygame.mouse.get_pos()
                 clicked = Game.click_loc(game1, pos)
                 if clicked:
                     game1.select(clicked[0], clicked[1])
-            elif key != None and game1.selected != None:
+            elif key != None and game1.selected != None: # update tiles with key presses
                 if game1.check_num(key):
                     game1.update_cube(key)
                 else:
                     game1.fail()
                 
                 key = None 
-        pygame.display.update()
+
+        pygame.display.update() # update screen
 
 main()
 pygame.quit()
 
-# Initial idea and GUI Inspiration From:
+# Initial idea and mild Inspiration From:
 # https://github.com/techwithtim/Sudoku-GUI-Solver
